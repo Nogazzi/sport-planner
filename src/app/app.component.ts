194c99f1111719e2dd6462c7cf3dcf0d7ff8455b
nogazzi@gmail.com <acai2020>
2021-05-20 16:04:38 +0200

    Add primeng

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,33 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {MenuItem} from 'primeng/api';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.sass']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'sport-planner';
+  items: MenuItem[];
+
+  constructor() {
+    this.items = [
+      {
+        label: 'Home',
+        icon: 'pi pi-fw pi-home'
+      },
+      {
+        label: 'Calendar',
+        icon: 'pi pi-fw pi-calendar'
+      },
+      {
+        label: 'Exercises',
+        icon: 'pi pi-fw pi-cog'
+      },
+
+    ];
+  }
+
+  ngOnInit(): void {
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,9 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import {MenubarModule} from 'primeng/menubar';
+import {PanelModule} from 'primeng/panel';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
 
 @NgModule({
   declarations: [
@@ -10,7 +13,10 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    MenubarModule,
+    BrowserAnimationsModule,
+    PanelModule
   ],
   providers: [],
   bootstrap: [AppComponent]
